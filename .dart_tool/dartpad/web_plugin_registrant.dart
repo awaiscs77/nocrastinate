eda2c02578a029d979a1da567e5f645fb93e23e9
Muhammad Awais <awais.cs77@gmail.com>
2026-02-15 02:58:40 +0500

    no message

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,44 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:app_links_web/app_links_web.dart';
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:desktop_webview_auth/desktop_webview_auth_web.dart';
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:google_sign_in_web/google_sign_in_web.dart';
+import 'package:package_info_plus/src/package_info_plus_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:share_plus/src/share_plus_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:sign_in_with_apple_web/sign_in_with_apple_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:wakelock_plus/src/wakelock_plus_web_plugin.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AppLinksPluginWeb.registerWith(registrar);
+  FirebaseFirestoreWeb.registerWith(registrar);
+  ConnectivityPlusWebPlugin.registerWith(registrar);
+  DesktopWebviewAuthWeb.registerWith(registrar);
+  FirebaseAnalyticsWeb.registerWith(registrar);
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  GoogleSignInPlugin.registerWith(registrar);
+  PackageInfoPlusWebPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  SharePlusWebPlugin.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  SignInWithApplePlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  WakelockPlusWebPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
